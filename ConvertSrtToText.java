98d7c8c518bb4b295fd00fab34a92ca5ceb896d5
StriveMoring <53570825+StriveMoring@users.noreply.github.com>
2023-04-07 11:45:17 +0800

    Add files via upload

---
+++ ConvertSrtToText.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+
+public class ConvertSrtToText {
+    public static void main(String[] args) {
+        String inputFile = "C:/Users/username/Desktop/example.srt";  // 输入文件路径
+        String outputFile = "C:/Users/username/Desktop/example.txt";  // 输出文件路径
+
+        try {
+            File input = new File(inputFile);
+            BufferedReader reader = new BufferedReader(new FileReader(input));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // 如果是序号或时间码，跳过
+                if (line.matches("\\d+") || line.matches("\\d{2}:\\d{2}:\\d{2},\\d{3}\\s-->\\s\\d{2}:\\d{2}:\\d{2},\\d{3}") || "".equals(line)) {
+                    continue;
+                }
+                writer.write(line + "\n");  // 写入文本信息
+            }
+
+            reader.close();
+            writer.close();
+            System.out.println("Done.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
